6e451fe951f0933c94675a0d0d9f889d17cc3421
gedesurya125 <gedesurya125@gmail.com>
2021-03-07 23:30:36 +0800

    backup databases

---
+++ programmer.sql
@@ -0,0 +1,77 @@
+-- MariaDB dump 10.19  Distrib 10.5.9-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: programmer
+-- ------------------------------------------------------
+-- Server version	10.5.9-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `skill_tb`
+--
+
+DROP TABLE IF EXISTS `skill_tb`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `skill_tb` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `skill_name` varchar(100) DEFAULT NULL,
+  `user_id` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `skill_tb`
+--
+
+LOCK TABLES `skill_tb` WRITE;
+/*!40000 ALTER TABLE `skill_tb` DISABLE KEYS */;
+INSERT INTO `skill_tb` VALUES (1,'php','1'),(2,'html','1'),(3,'css','1'),(4,'ReactJS','2'),(5,'React Native','2'),(6,'Express','2'),(7,'PHP','3'),(8,'html','3'),(9,'css','3');
+/*!40000 ALTER TABLE `skill_tb` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_tb`
+--
+
+DROP TABLE IF EXISTS `user_tb`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_tb` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `photo` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_tb`
+--
+
+LOCK TABLES `user_tb` WRITE;
+/*!40000 ALTER TABLE `user_tb` DISABLE KEYS */;
+INSERT INTO `user_tb` VALUES (1,'nunchaku',NULL),(2,'holy knight escanor',NULL),(3,'the grizly',NULL);
+/*!40000 ALTER TABLE `user_tb` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-07 23:28:42
